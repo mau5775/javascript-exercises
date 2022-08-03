b1601701170e55599a0e899419068f9a163ebaa6
mau5775 <mohd.arif.5775@gmail.com>
2022-08-03 18:14:40 +0400

    Made the function to test all conditions!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
+    let str1="";
 
+    if(num<0){
+        return 'ERROR';
+    }
+    else {
+    for(let i=0;i<num;i++)
+    {
+        str1+=str;
+        
+    }
+    }
+    return str1;
 };
 
 // Do not edit below this line
