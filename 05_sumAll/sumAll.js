f00008f6a77d50fdf242b2919c6f25f1e47fac5a
mau5775 <mohd.arif.5775@gmail.com>
2022-08-04 20:09:51 +0400

    Function passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    let tsum=0;
+   if(typeof n1 !== 'number' || typeof n2 !== 'number'){
+    return 'ERROR';
+   }
+   else if(n1<0 || n2<0){
+            return 'ERROR';
+        }
+        else if(n1<n2){
+                for(let i=n1;i<=n2;i++){
+                    tsum+=i;
+                }
+            }
+            else
+            {
+                for(let i=n1;i>=n2;i--){
+                    tsum+=i;
+                }
+            }
+    return tsum;
 };
 
 // Do not edit below this line
