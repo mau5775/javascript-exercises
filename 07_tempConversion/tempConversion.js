bb85ce6e15bccd621b8b589bc4faa23e2800a15f
mau5775 <mohd.arif.5775@gmail.com>
2022-08-04 20:51:42 +0400

    CTOF FTOC works all conditions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+
+  let cel = (temp - 32) *5/9;
+  celsius = Math.round(cel * 10) / 10
+
+  return celsius
 
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+
+  let far = (temp*9/5) + 32;
+  farenhiet = Math.round(far*10)/10;
+
+  return farenhiet;
 
 };
 
