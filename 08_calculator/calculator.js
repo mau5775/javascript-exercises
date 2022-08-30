420b5cbeeefd54bf8cf92965a166eeb6c29de08c
mau5775 <mohd.arif.5775@gmail.com>
2022-08-30 15:59:41 +0400

    Calculator works

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,60 @@
-const add = function() {
+const add = function(...args) {
+	let total =0;
+  for(let i=0;i<args.length;i++){
+    total+=args[i];
+    }
+    return total;
+};
+
+const subtract = function(...args) {
+	let total = args[0];
+  for(let i=1;i<args.length;i++){
+    total= total - args[i];
+    }
+    
+    return total;
+};
+
+const sum = function(...args) {
+
+  let total =0;
+  let arrayArgs = args[0];
+  
+  for(let i=0;i<arrayArgs.length;i++){
+    total+=arrayArgs[i];
+    }
+  
+    return total;
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(...args) {
+  let total=1;
+  let fargs = args[0];
+
+  for(let i=0;i<fargs.length;i++){
+    total = total*fargs[i];
+  }
+  return total;
 };
 
-const sum = function() {
-	
+const power = function(...args) {
+	let total = 0;
+  total = Math.pow(args[0],args[1]);
+  return total;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(a) {
+  let total = 1;
+  if(a===0){
+    return 1;
+  }
+  else {
+    for(let i=1;i<=a;i++){
+      total*=i;
+    }
+    return total;
+  }
 	
 };
 
