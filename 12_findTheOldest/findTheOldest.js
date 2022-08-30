7b5a66b01c0571887557cca18c4bfe8450c2a9f9
mau5775 <mohd.arif.5775@gmail.com>
2022-08-30 22:22:50 +0400

    works all test cases!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let dt = new Date();
+    let ndt = dt.getFullYear();
+    
+    for(let i=0;i<people.length;i++){
+      let obj = people[i];
+      
+        if(!('yearOfDeath' in obj)){
+          obj['yearOfDeath'] = ndt;
+          console.log(`Defining year for ${obj['name']}`);
+        }       
+    }
+    
+    const oldestName = people.sort((a,b) => {
+        const lastInventor = a.yearOfDeath - a.yearOfBirth;
+        const nextInventor = b.yearOfDeath - b.yearOfBirth;
+        return lastInventor > nextInventor ? -1 : 1;
+    });
+    console.log(oldestName);
+    let oldestPerson = oldestName[0];
+    return oldestPerson;
 };
 
 // Do not edit below this line
