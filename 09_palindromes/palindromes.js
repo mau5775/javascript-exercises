0c60e7c041399565f08044b5810e17c478befa29
mau5775 <mohd.arif.5775@gmail.com>
2022-08-30 16:30:01 +0400

    function works for all TCs

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[^a-zA-Z0-9]/g, '');
+    let str2 = str.split("").reverse().join("");
+    str = str.toLowerCase();
+    str2 = str2.toLowerCase();
+
+    if(str === str2)
+        return true;
+        else
+        return false;
+
 
 };
 
