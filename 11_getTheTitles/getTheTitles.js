21a0245b604ea0951d5481533cf8825516f1c9c4
mau5775 <mohd.arif.5775@gmail.com>
2022-08-30 20:34:57 +0400

    returned the new array of titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const getTitle = books.map(book=> `${book.title}`);
+    return getTitle;
 };
 
 // Do not edit below this line
